3e32a8aad415e8f3eeb4ef503057119a7ae06e24
d11416148-sys <d11416148@gs.takming.edu.tw>
2026-06-02 14:37:37 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Person
+{
+    public static int GetName(int a)
+    {
+        return a;
+    }
+
+    public static string GetName(int a, int b)
+    {
+        return a + b + "OK!";
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Class_0602_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person p1 = new Person();
+
+            Console.WriteLine(Person.GetName(1));
+            Console.WriteLine(Person.GetName(1, 6));
+        }
+    }
+}
